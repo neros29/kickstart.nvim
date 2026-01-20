23de0e6193413ac183a3497f0b852401483b0c4c
neros29 <sorenmile29@gmail.com>
2026-01-20 13:28:34 -0700

    changed harpoon config

--- lua/neros/plugins/harpoon.lua
+++ lua/neros/plugins/harpoon.lua
@@ -8,9 +8,21 @@ return {
       harpoon:setup()
 
       -- Add file
-      vim.keymap.set('n', '<leader>a', function()
+      vim.keymap.set('n', '<leader>at', function()
+        harpoon:list():prepend()
+      end, { desc = 'Harpoon add file to list top' })
+
+      -- Add file
+      vim.keymap.set('n', '<leader>ab', function()
         harpoon:list():add()
-      end, { desc = 'Harpoon add file' })
+      end, { desc = 'Harpoon add file to list bottom' })
+
+      -- add to line 1–9
+      for i = 1, 9 do
+        vim.keymap.set('n', '<leader>a' .. i, function()
+          harpoon:list():replace_at(i)
+        end, { desc = 'Harpoon add file to line' .. i })
+      end
 
       -- Toggle menu
       vim.keymap.set('n', '<leader>h', function()
@@ -21,13 +33,13 @@ return {
       for i = 1, 9 do
         vim.keymap.set('n', '<leader>' .. i, function()
           harpoon:list():select(i)
-        end, { desc = 'Harpoon file ' .. i })
+        end, { desc = 'Harpoon get file at line ' .. i })
       end
 
       -- 0 → 10
       vim.keymap.set('n', '<leader>0', function()
         harpoon:list():select(10)
-      end, { desc = 'Harpoon file 10' })
+      end, { desc = 'Harpoon get file line at 10' })
     end,
   },
 }
